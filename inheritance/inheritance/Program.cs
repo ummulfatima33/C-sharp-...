d5ab3143c1bc6e4e1428d817d5dda3f5513a60c5
ummul fatima <ummulf02@gmail.com>
2025-08-23 15:15:51 -0700

    Add files via upload

---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //========Object Vehicle Class=========
+            //Vehicle vehicle = new Vehicle();
+            //vehicle.vModel = "Yaris";
+            //vehicle.vYear = "2025";
+            //vehicle.print();
+
+            //========Object Toyota Class=========
+            Toyota toyota = new Toyota();
+            toyota.vModel = "Yaris";
+            toyota.vYear = "2025 Latest";
+            toyota.reviews = "5 stars";
+            toyota.detailPrint();
+
+            Console.WriteLine("-------------------------------------------------------------");
+            //========Object Toyota Class=========
+            Audi audi = new Audi();
+            audi.vModel = "A4";
+            audi.vYear = "2022";
+            audi.luxury = "10 out of 10";
+            audi.Detail();
+
+        }
+    }
+
+    class Vehicle
+    {
+        public string vModel;
+        public string vYear;
+
+
+            //**********Method Vehicle Class***********
+                //public void print()
+        //{
+        //    Console.WriteLine("model is {0} & Year {1}", this.vModel, this.vYear);
+        //}
+    }
+
+    class Toyota : Vehicle
+    {
+        public string reviews;
+
+        //**********Method Toyata Class***********
+        public void detailPrint()
+        {
+            Console.WriteLine("model is {0} , manufacturing Year {1} & {2} rating...",
+                this.vModel, this.vYear, this.reviews);
+        }
+    }
+
+    //**********Method Audi Class***********
+    class Audi : Vehicle
+    {
+        public string luxury;
+
+        public void Detail()
+        {
+            Console.WriteLine("model is {0} , manufacturing Year {1} & {2} rating...",
+                this.vModel, this.vYear, this.luxury);
+        }
+
+    }
+}
---
+++ multiLevel/multiLevel/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace multiLevel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Cat mycat = new Cat();
+            mycat.name = "Twinkle";
+            mycat.sound = "Meon Meon";
+            mycat.colour = "White";
+            mycat.Details();
+
+        }
+    }
+
+
+    class Animal
+    {
+        public string name;
+        public string sound;
+       }
+
+
+     class Mammals : Animal
+    {
+      
+    }
+
+    class Cat : Mammals
+    {
+        public string colour;
+        public void Details()
+        {
+            Console.WriteLine("Cat name is {0}, Sound {1} & colour is {2}...",
+                this.name, this.sound, this.colour);
+        }
+
+
+    }
+
+
+}
