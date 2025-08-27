7d930bca2e822fd4d572c58e85824dda7c2e1c25
ummul fatima <ummulf02@gmail.com>
2025-08-27 04:06:50 -0700

    Add files via upload

---
+++ polymorphism/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class Animal
+    {
+        virtual public void Sound()
+        {
+            Console.WriteLine( "Animal Sound");
+        }
+    }
+
+    class Cat : Animal
+    {
+       override public void Sound()
+        {
+            Console.WriteLine("Meaon Meaon");
+        }
+    }
+    class Dog : Animal
+    {
+      override  public void Sound()
+        {
+            Console.WriteLine("Baooo Baooo");
+        }
+    }
+
+
+          //********sealed Classes*******
+    //sealed class A
+    //{
+    //    public string Name = "Class A";
+    //}
+
+    //class B : A
+    //{
+    //    public string name = "Class B";
+    //}
+
+}
---
+++ polymorphism/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Compile Time Polymorphism Objects
+            Calculator mycalculator = new Calculator();
+            mycalculator.Calculate(12, 3);
+            mycalculator.Calculate(2, 1.5f, 2);
+            mycalculator.Calculate("Fatima", "Shahid", "Attari");
+            mycalculator.Calculate(5.5f, 1.5f, 0.2f);
+
+
+            //Compile Time Polymorphism Objects
+             Cat mycat = new Cat();
+             mycat.Sound();
+
+            Dog mydog = new Dog();
+            mydog.Sound();
+
+           
+
+        }
+
+
+        //Compile Time Polymorphism
+
+        class Calculator
+        {
+            public void Calculate(int a , int b)
+            {
+                Console.WriteLine( a + b);
+            }
+
+            public void Calculate(int a, float b, int c)
+            {
+                Console.WriteLine(a - b * c);
+            }
+
+            public void Calculate(string a, string b, string c)
+            {
+                Console.WriteLine(a + " " + " " + b + " " + c);
+            }
+
+            public void Calculate(float a, float b, float c)
+            {
+                Console.WriteLine(a * b % c);
+            }
+        }
+
+    }
+}
