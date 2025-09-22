04ab293c2008b544a72ed54082acb473d6fca1a6
ummul fatima <ummulf02@gmail.com>
2025-09-22 12:46:39 -0700

    Add files via upload

---
+++ NameSpace/Program.cs
@@ -0,0 +1,42 @@
+﻿using Namespace2;
+using FileLevel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSpace
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+        {
+                 //NameSpace 1 Objects
+            Students students = new Students();
+            students.name("Ali Raza");
+
+            Students students1 = new Students();
+            students1.name("Yaqoob");
+
+            Teacher teacher = new Teacher();
+            teacher.course("ADSE");
+
+            //NameSpace 2 Objects
+
+            Namespace2.Teacher teacher1 = new Namespace2.Teacher();
+            teacher1.tname("Sir Rizwan");
+
+            Subject subject = new Subject();
+            subject.module(3);
+
+
+            //FileLevel Objects
+
+            ClassA obj = new ClassA();
+          
+
+        }
+    }
+}
+            
---
+++ NameSpace/Students.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSpace
+{
+    internal class Students
+    {
+        public void name(string sname)
+        {
+            Console.WriteLine("Name is " + sname);
+        }
+    }
+
+    class Teacher
+    {
+        public void course(string course)
+        {
+            Console.WriteLine("Enroll  " + course);
+        }
+    }
+}
+
+namespace Namespace2
+{
+    class Student
+    {
+
+    }
+ class Teacher
+    {
+        public void tname(string fname)
+        {
+            Console.WriteLine("Teacher Name Is  " + fname);
+        }
+    }
+    class Subject
+    {
+        public void module(int module)
+        {
+            Console.WriteLine("Module Version  " + module);
+        }
+    }
+}
